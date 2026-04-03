cf2df8cee63f8c9ce4042f1919fae4a96d66a10a
Magnus Jonsson <fot@fot.nu>
2026-04-03 15:17:39 +0200

    Fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 GH_OWNER=$GH_OWNER
 GH_REPOSITORY=$GH_REPOSITORY
 GH_TOKEN=$GH_TOKEN
-GH_REG_TOKEN=$GH_REG_TOKEN=
+GH_REG_TOKEN=$GH_REG_TOKEN
 
 RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
 RUNNER_NAME="${REAL_HOSTNAME:-dependaNode}-${RUNNER_SUFFIX}"
@@ -11,7 +11,7 @@ RUNNER_NAME="${REAL_HOSTNAME:-dependaNode}-${RUNNER_SUFFIX}"
 cd /home/docker/actions-runner
 
 if ! [[ -f /secrets/.credentials ]]; then
-	if [[ -z "${GH_REG_TOKEN}" ]]; then
+	if [[ -z "${GH_REG_TOKEN:-}" ]]; then
 		GH_REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 	fi
 
@@ -29,7 +29,7 @@ ln -s /secrets/.credentials* .
 cleanup() {
     echo "exit runner..."
     kill -HUP $pid
-    # ./config.sh remove --unattended --token ${REG_TOKEN}
+    # ./config.sh remove --unattended --token ${GH_REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
